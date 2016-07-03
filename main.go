4d25473d2fbf2aebf4c659ce21a06f558a5a5298
= <sethhandy39@gmail.com>
2016-07-03 19:30:33 -0400

    Revert "added echo and have ticker for tick data on book trades"
    This reverts commit cb371854acb34960af4bdbab0877b8059004cf35.

--- main.go
+++ main.go
@@ -1,53 +1,63 @@
 package main
 
 import (
-	//"github.com/Mummie/Bitcoin_Trader/market_feed/trade"
-	//"github.com/labstack/echo"
-	"github.com/bitfinexcom/bitfinex-api-go"
-	//"github.com/labstack/echo/engine/standard"
+	"fmt"
+	//"github.com/bitfinexcom/bitfinex-api-go"
+	"io/ioutil"
 	"log"
+	"net/http"
+	//"os"
+	"time"
 )
 
+type Config struct {
+	API_KEY    string
+	API_SECRET string
+}
+
+type TickerData struct {
+	Mid       string `json:"mid"`
+	Bid       string `json:"bid"`
+	Ask       string `json:"ask"`
+	LastPrice string `json:"last_price"`
+	Low       string `json:"low"`
+	High      string `json:"high"`
+	Volume    string `json:"volume"`
+	Timestamp string `json:"timestamp"`
+}
+
+// GET gets tick data or symbol
+func Ticker(symbol string) string {
+
+	req, err := http.NewRequest("GET", "https://api.bitfinex.com/v1/pubticker/LTCUSD", nil)
+
+	if err != nil {
+
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(body)
+}
+
 func main() {
-	/*e := echo.New()
-	e.GET("/ticker", trade.Ticker)
-	e.Run(standard.New(":8008"))
-	log.Println("Server running on 8008")
-	*/
-	api := bitfinex.NewClient()
-	// Create new connection
-	e := api.WebSocket.Connect()
 
-	if e != nil {
-		log.Fatal("Error connecting to web socket")
-
+	for i := 1; i < 1000; i++ {
+		time.Sleep(3000 * time.Millisecond)
+		fmt.Println(Ticker("BTCUSD"))
 	}
-	defer api.WebSocket.Close()
 
-	book_btcusd_chan := make(chan []float64)
-	book_ltcusd_chan := make(chan []float64)
-	trades_chan := make(chan []float64)
-	ticker_chan := make(chan []float64)
-
-	api.WebSocket.AddSubscribe(bitfinex.CHAN_BOOK, bitfinex.BTCUSD, book_btcusd_chan)
-	api.WebSocket.AddSubscribe(bitfinex.CHAN_BOOK, bitfinex.LTCUSD, book_ltcusd_chan)
-	api.WebSocket.AddSubscribe(bitfinex.CHAN_TRADE, bitfinex.BTCUSD, trades_chan)
-	api.WebSocket.AddSubscribe(bitfinex.CHAN_TICKER, bitfinex.BTCUSD, ticker_chan)
-	go api.WebSocket.Subscribe()
-
-	// after api client successfully connect to remote web socket
-	// channel will reveive current payload as separate messages.
-	// each channel will receive order book updates: [price, count, ±amount]
-	for {
-		select {
-		case btcusd_msg := <-book_btcusd_chan:
-			log.Println("BOOK BTCUSD:", btcusd_msg)
-		case ltcusd_msg := <-book_ltcusd_chan:
-			log.Println("BOOK LTCUSD:", ltcusd_msg)
-		case trade_msg := <-trades_chan:
-			log.Println("TRADES:", trade_msg)
-		case ticker_msg := <-ticker_chan:
-			log.Println("TICKER:", ticker_msg)
-		}
-	}
 }
